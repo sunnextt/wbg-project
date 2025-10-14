eee86f197e77b96851a673a85ae9b5ced1bb5d6f
sunnextt <sunnext2010@gmail.com>
2025-10-14 09:27:05 +0100

    working or deploy issues

--- Server/models/User.js
+++ Server/models/User.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  uid: { type: String, required: true, unique: true }, // Firebase UID
+  uid: { type: String, required: true, unique: true },
   email: { type: String, required: true },
   username: { type: String, default: '' },
   Fullname: { type: String, default: '' },
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
-  online: { type: Boolean, default: false }, // Added for chat
+  online: { type: Boolean, default: false },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
